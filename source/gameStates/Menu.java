87f4dd7b9032762fee87669322100fc9139b7808
Xaphier2000 <Marco.Schubbach@gmail.com>
2017-07-22 01:05:29 +0200

    Mainmenue changed Account Support added

--- source/gameStates/Menu.java
+++ source/gameStates/Menu.java
@@ -6,9 +6,11 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import connections.mysqlconn;
 import gui.Button;
 import tools.Loader;
 import tools.States;
@@ -19,6 +21,10 @@ public class Menu extends BasicGameState{
 	Image background;
 	Image magigity;
 	Loader loader = new Loader();
+	TextField name;
+	TextField pw;
+	Button submit;
+	
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		
@@ -30,13 +36,41 @@ public class Menu extends BasicGameState{
 							Display.getWidth()/2-100,Display.getHeight()/2-50,200,100);
 		magigity = loader.loadImage("Magigity");
 		background = loader.loadImage("MenuBackground");
+		
+        name = new TextField(gc, gc.getDefaultFont(), Display.getWidth()/2-50, Display.getHeight()/2-40, 100, 30);
+        pw = new TextField(gc, gc.getDefaultFont(), Display.getWidth()/2-50, Display.getHeight()/2, 100, 30);
+        name.setText("Gamename");
+        pw.setText("Password");
+        submit = new Button("Submit", loader.loadImage("basicbutton_pressed"), loader.loadImage("basicbutton_released"),
+        					Display.getWidth()/2-100,Display.getHeight()/2+50,200,100);
+		
 	}
 	
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {	
-		start.update(input);
-		if(start.isPressedAndReleased())
-			sbg.enterState(States.running);
+		//start.update(input);
+		//if(start.isPressedAndReleased())
+			//sbg.enterState(States.running);
+		
+		
+		submit.update(input);
+		if(submit.isPressedAndReleased()) {
+			
+			boolean pass = mysqlconn.checkPw(name.getText(),pw.getText());
+			System.out.println(pass);
+			if(pass == true) {
+				
+				sbg.enterState(States.running);
+				
+			}
+			else {
+				
+				System.out.println("Wrong Combination, Dude");
+				
+			}
+			
+		}
+		
 		background.rotate(0.01f*delta);
 	}
 	
@@ -45,7 +79,10 @@ public class Menu extends BasicGameState{
 		background.drawCentered(Display.getWidth()/2, Display.getHeight()/2);
 		
 		magigity.drawCentered(Display.getWidth()/2, magigity.getHeight()/2+10);
-		start.render(g);
+		//start.render(g);
+		name.render(gc, g);
+		pw.render(gc, g);
+		submit.render(g);
 	}
 
 	@Override
